1b2d78224eab8ef07930e5b74773f2281276b589
aisyah-h <83572065+aisyah-h@users.noreply.github.com>
2021-05-25 09:18:21 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 //extension .js in math.js kept just for ease of reference
-//ONLY for default: can actually name the object differently from what it's named in math.js, as long as use the same name in this file
+//ONLY for default: can actually name the object differently from what it's named in math.js, as long as used consistently in this file
 //can also use wildcard, but discouraged (refer to Notion notes for example, under Section 33.6)
 import pie, { doublePi, triplePi } from "./math.js";
 
